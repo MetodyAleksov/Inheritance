1aff5197e08867859a9c3cf1f5894b36f9c7fcee
Teddy Aleksov <teddy57603@gmail.com>
2021-02-23 21:16:18 +0200

    updated

--- 04.RandomList/Program.cs
+++ 04.RandomList/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CustomRandomList
 {
@@ -9,9 +8,5 @@ namespace CustomRandomList
         {
             Console.WriteLine("Hello World!");
         }
-    }
-    public class RandomList<T> : List<T>
-    {
-
     }
 }
---
+++ 04.RandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, base.Count - 1);
+
+            string item = base[index];
+
+            base.RemoveAt(index);
+
+            return item;
+        }
+    }
+}
