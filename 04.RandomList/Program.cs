9638d82adc9155a37215b54924b41362295cc619
Teddy Aleksov <teddy57603@gmail.com>
2021-02-23 20:57:31 +0200

    updated

---
+++ 04.RandomList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class RandomList<T> : List<T>
+    {
+
+    }
+}
