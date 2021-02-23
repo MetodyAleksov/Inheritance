c5b95a883f113841fbb58dfd4232eba1614ea337
Teddy Aleksov <teddy57603@gmail.com>
2021-02-23 21:41:12 +0200

    updated

---
+++ 05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (base.Count != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
